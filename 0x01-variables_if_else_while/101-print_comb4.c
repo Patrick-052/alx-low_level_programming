540a74adaa5268d1d4c407e4f2c781ef967a47cf
Patrick-052 <muiruripatrick052@gmail.com>
2022-12-11 09:20:11 -0800

    success

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int x, y, z;
+for (x = 48; x < 58; x++)
+{
+for (y = 49; y < 58; y++)
+{
+for (z = 50; z < 58; z++)
+{
+if (y > x && z > y)
+{
+putchar(x);
+putchar(y);
+putchar(z);
+if (x != 55 || y != 56 || z != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
