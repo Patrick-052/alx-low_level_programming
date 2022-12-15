3ac5e005ebfadf6eeefd77b021d37c48d5066433
Patrick-052 <muiruripatrick052@gmail.com>
2022-12-15 07:22:57 -0800

    numbers git commit -m number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - function that prints the numbers, from 0 to 9,
- * followed by a new line
- * Return: Always 0 (success)
+ * print_numbers - print 0123456789
+ *
+ * Return: void
  */
 void print_numbers(void)
 {
 	char c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
