b7ab53a7d1a385e4d14b4c1d6c70b2e386ed7468
Patrick-052 <muiruripatrick052@gmail.com>
2022-12-19 06:14:07 -0800

    Half

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,6 +3,7 @@
 /**
  * puts2 - function that prints every other character of a string
  * starting with the first character, followed by a new line
+ * @str: string
  * Return: Always 0
  */
 void puts2(char *str)
