a759a9e5f86ff1e0be86c5338e375843402fbac2
Patrick-052 <muiruripatrick052@gmail.com>
2023-01-14 13:43:31 -0800

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,66 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _realloc - function reallocates a memory block using malloc
- * and free
+ * _realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previously allocated
- * @old_size: size in bytes
- * @new_size: size in bytes
- * Return: pointer
+ * @old_size:is the size, in bytes, of the allocated space for ptr
+ * @new_size: the new size, in bytes of the new memory block
+ * Return: pointer allocate new size memory, or NULL
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *reaptr;
+	char *p;
+	unsigned int i, n = new_size;
+	char *oldp = ptr;
 
 	if (ptr == NULL)
 	{
-		reaptr = malloc(new_size);
-		if (reaptr == NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		free(ptr);
-		return (reaptr);
+		p = malloc(new_size);
+		return (p);
 	}
-	if (new_size == old_size)
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
+	p = malloc(new_size);
+	if (p == NULL)
 		return (NULL);
-	}
-	reaptr = malloc(new_size);
-	if (reaptr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
 	if (new_size > old_size)
-		_memcpy(reaptr, ptr, old_size);
+		n = old_size;
+	for (i = 0; i < n; i++)
+		p[i] = oldp[i];
 	free(ptr);
-	return (reaptr);
-}
-
-/**
- * _memcpy - function that copies memory area
- * @dest: dest positions
- * @src: source position
- * @n: size of bytes
- * Return: char
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i = 0;
-	unsigned int j = 0;
-
-	while (i < n)
-	{
-		*(dest + i) = *(src + j);
-		i += 1;
-		j += 1;
-	}
-	return (dest);
+	return (p);
 }
