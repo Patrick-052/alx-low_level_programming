bc42b956bdf8f372a8393d30b94321ce41c01a60
Patrick-052 <muiruripatrick052@gmail.com>
2023-02-10 01:15:34 -0800

    Under the snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
