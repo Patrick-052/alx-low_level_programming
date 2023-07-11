0d49fbefdd60f8d63b55d0e0ca0ef69273c696d0
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-11 18:39:44 +0300

    search algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef search_algos_h
+#define search_algos_h
+
+#include <stdio.h>
+
+int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
